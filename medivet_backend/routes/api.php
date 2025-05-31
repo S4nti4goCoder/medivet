bbc0a6e3e2db163c1c33175ee1f80427727dd55c
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-05-30 19:32:10 -0500

    build(projects): The project structure is created, the backend is created with laravel and the frontend with vue, jwt is installed, testing is created in insomnia and the first registration and login end points are created, laravel permission is installed and laravel permission is tested in the backend.

---
+++ medivet_backend/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::group([
+    // 'middleware' => 'api',
+    'prefix' => 'auth',
+    'middleware' => ['auth:api'] //'permission:edit articles'
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); //->middleware('auth:api')
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh'); //->middleware('auth:api')
+    Route::post('/me', [AuthController::class, 'me'])->name('me'); //->middleware('auth:api')
+});
